927cf3a437aad2ead499c2182dfc7452db33dc18
Shen pei <shen.pei.wun@gmail.com>
2022-10-26 11:59:57 +0800

    1026

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Counter from './ex-1025/Counter'
+// import Counter from './ex-1025/Counter'
+//import JsxValue from './ex-1026/JsxValue'
+import StudentList from "./ex-1026/StudentList"
 
 function App() {
   return (
     <>
-      <Counter />
+      <StudentList />
     </>
   )
 }
