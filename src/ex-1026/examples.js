887e9e5369b6ab25d0026b9b40d9e1670c9fc0a6
Shen pei <shen.pei.wun@gmail.com>
2022-10-26 15:04:55 +0800

    1026-下拉式選單

---
+++ src/ex-1026/examples.js
@@ -0,0 +1,21 @@
+import Counter from '../ex-1025/Counter'
+import JsxValue from './JsxValue'
+import StudentList from './StudentList'
+
+export const examples = [
+  {
+    name: '1025計數器',
+    path: '../ex-1025/Counter',
+    component: Counter,
+  },
+  {
+    name: '1026JSX中的值與表達式',
+    path: './JsxValue',
+    component: JsxValue,
+  },
+  {
+    name: '1026 學生列表清單(map-key)',
+    path: './StudentList',
+    component: StudentList,
+  },
+]
