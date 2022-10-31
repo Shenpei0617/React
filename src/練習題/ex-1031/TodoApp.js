f11eb41e5d2961bd6670d3f56d9043ab9ef3f11f
Shen pei <shen.pei.wun@gmail.com>
2022-10-31 13:44:46 +0800

    1031-待辦事項2-newTodoId

--- src/練習題/ex-1031/TodoApp.js
+++ src/練習題/ex-1031/TodoApp.js
@@ -16,8 +16,10 @@ function TodoApp() {
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
             // 加入輸入的文字到todos陣列中
-            // 三步驟的方式(拷貝 -> 加入 -> 設定回state)
-            const newTodos = [e.target.value, ...todos]
+            // 三步驟的方式(拷貝 ->  加入到新陣列中 -> 設定回state)
+            // id: Number(new Date()) 把ID設為現在加入的時間
+            const newTodoId = { id: Number(new Date()), text: e.target.value }
+            const newTodos = [newTodoId, ...todos]
             setTodos(newTodos)
 
             // 清空文字輸入框
@@ -27,7 +29,8 @@ function TodoApp() {
       />
       <ul>
         {todos.map((v, i) => {
-          return <li key={i}>{v}</li>
+          // 因為TODO不像日期一樣是固定的，所以key值會因索引值變後會變化，不能用索引值當key
+          return <li key={v.id}>{v.text}</li>
         })}
       </ul>
     </>
