457ca43832991be896b82c32412806751c32f971
Shen pei <shen.pei.wun@gmail.com>
2022-11-02 16:03:25 +0800

    1102-.從伺服器載入資料-載入指示動畫

--- src/ex-1102/data/User.js
+++ src/ex-1102/data/User.js
@@ -8,44 +8,78 @@ function User() {
   //   "birth": "990101"
   // }]
   const [users, setUsers] = useState([])
+  // 載入資料指示
+  const [isLoading, setIsLoading] = useState(false)
+
+  //錯誤訊息用
+  const [errorMessage, setErrorMessage] = useState('')
 
   const getUsers = async () => {
-    const response = await axios.get(
-      'https://my-json-server.typicode.com/eyesofkids/json-fake-data/users'
-    )
-    console.log(response)
+    try {
+      const response = await axios.get(
+        'https://my-json-server.typicode.com/eyesofkids/json-fake-data/users'
+      )
+      console.log(response)
 
-    //設定到state裡
-    setUsers(response.data)
+      //設定到state裡
+      setUsers(response.data)
+    } catch (e) {
+      // 錯誤處理
+      console.error(e.message)
+      setErrorMessage(e.message)
+    }
   }
 
   // didMount時載入資料
   useEffect(() => {
+    //先載入指示器
+    setIsLoading(true)
+
     getUsers()
+
+    // 延後1.5秒才關掉指示器
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
   }, [])
+
+  const spinner = (
+    <>
+      <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </>
+  )
+
+  const display = errorMessage ? (
+    errorMessage
+  ) : (
+    <div className="bd-example">
+      <table className="table table-dark table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">姓名</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((v, i) => {
+            return (
+              <tr key={v.id}>
+                <td>{v.id}</td>
+                <td>{v.name}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
   return (
     <>
       <h1>會員資料</h1>
-      <div class="bd-example">
-        <table class="table table-dark table-striped">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">姓名</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((v, i) => {
-              return (
-                <tr key={v.id}>
-                  <td>{v.id}</td>
-                  <td>{v.name}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+      <hr />
+      {isLoading ? spinner : display}
     </>
   )
 }
