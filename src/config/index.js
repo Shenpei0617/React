7a13c842f19b2e4aac0ebcf15453718b83639cc5
Shen pei <shen.pei.wun@gmail.com>
2022-11-03 10:51:31 +0800

    1103-圖片引入-圖片在node(或其它伺服器上)的public中

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,10 @@ export const appConfig = {
   debug: true,
   devUrl: 'http://localhost:3000',
   prodUrl: 'https://www.abc.com',
+  devServerUrl: 'http://localhost:5555',
 }
 export const imgUrl = appConfig.debug ? appConfig.devUrl : appConfig.prodUrl
+
+export const imgServerUrl = appConfig.debug
+  ? appConfig.devServerUrl
+  : appConfig.prodUrl
--- src/ex-1103/ImageDemo.js
+++ src/ex-1103/ImageDemo.js
@@ -1,5 +1,5 @@
 import LogoImg from './logo.svg'
-import { imgUrl } from '../config'
+import { imgUrl, imgServerUrl } from '../config'
 
 function ImageDemo() {
   return (
@@ -20,6 +20,35 @@ function ImageDemo() {
         //  套用 .env檔中的設定值(須重啟圖片才會顯示)
         // https://create-react-app.dev/docs/adding-custom-environment-variables/
         src={`${process.env.REACT_APP_DEV_URL}/images/logo192.png`}
+        alt=""
+        width="100"
+        height="100"
+      />
+      <p>3.圖片在node(或其他伺服器上)的public中，用絕對網址存取</p>
+      <img
+        // 套用config/index.js中的設定值
+        src={`${imgServerUrl}/uploads/anya-300-03.png`}
+        alt=""
+        width="100"
+        height="100"
+      />
+      <img
+        //  套用 .env檔中的設定值
+        // https://create-react-app.dev/docs/adding-custom-environment-variables/
+        src={`${process.env.REACT_APP_DEV_SERVER_URL}/uploads/anya-300-03.png`}
+        alt=""
+        width="100"
+        height="100"
+      />
+      <p>
+        4. 圖片在node(或其它伺服器上)的public中，用第3種的伺服器方式存取(!!注意:
+        開發用，而且需先設定proxy)
+      </p>
+      {/* // 另外安裝 `yarn add http-proxy-middleware` 
+          https://create-react-app.dev/docs/proxying-api-requests-in-development/
+      */}
+      <img
+        src={`${imgUrl}/uploads/anya-300-03.png`}
         alt=""
         width="100"
         height="100"
